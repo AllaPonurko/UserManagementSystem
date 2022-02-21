80217ff884762342afa72151ee3f2c481a4b07e3
AllaPonurko <96493346+AllaPonurko@users.noreply.github.com>
2022-02-21 19:53:33 +0200

    Add files via upload

---
+++ UserManagementSystem/Data/Entities/Role.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem.Data.Entities
+{
+    /// <summary>
+    /// Сущность доступных ролей системы
+    /// </summary>
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ UserManagementSystem/Data/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem.Data.Entities
+{ 
+    {
+        /// <summary>
+        /// Сущность пользователя системы
+        /// </summary>
+        public class User
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public Role Role { get; set; }
+            public List<Role> Roles { get; set; } = new List<Role>();
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+    }
+
+}
